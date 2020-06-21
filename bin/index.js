60f8d15e6dc0a9eaf95286dfbe4ebff69a6358a7
Dan <dmcg926@gmail.com>
2020-06-20 21:25:45 -0500

    renamed capitalize function...totally necessary...

--- bin/index.js
+++ bin/index.js
@@ -6,13 +6,13 @@ const getWeather = require('../lib/weather.js');
 var args = process.argv.splice(process.execArgv.length + 2);
 
 // takes in a city from first argument, or set a default (spaces must be _ w/ first letters captilized)
-let city = args[0] === undefined ? 'Chicago' : capitalizeFirst(args[0]);
+let city = args[0] === undefined ? 'Chicago' : capitalize(args[0]);
 // takes in state from second argument or uses default if undefined (spaces must be _ w/ first letters captilized)
-let state = args[1] === undefined ? 'Illinois' : capitalizeFirst(args[1]);
+let state = args[1] === undefined ? 'Illinois' : capitalize(args[1]);
 
 // console.log(city, state);
 
-function capitalizeFirst(string) {
+function capitalize(string) {
   if (string.includes('_')) {
     let str_arr = string.split('_');
 
